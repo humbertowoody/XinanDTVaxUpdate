b858f8f8e8d983de44a19c96f63e34ce0068fc31
Humberto Alcocer <humbertowoody@gmail.com>
2018-05-29 23:57:52 -0500

    Update de estilo

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const sql = require('mssql');
 
 // Configuración de conexión a Microsoft SQL Server
 const config = {
-    user: 'node',
-    password: '12345678',
-    server: 'localhost\\sqlexpress',
-    database: 'dbSistema',
-    encrypt: false,
+  user: 'node',
+  password: '12345678',
+  server: 'localhost\\sqlexpress',
+  database: 'dbSistema',
+  encrypt: false,
 }
 
 // Número de Días de atraso en la información
@@ -31,46 +31,44 @@ const tabla = 'tablaprueba';
 
 // Obtener un string con la fecha a utilizar.
 function getStringFecha() {
-    // Fecha con atraso calculado
-    const fechaConAtraso = moment().subtract(diasAtraso, 'days');
-    // Regresamos string con fecha en formato yy/mm/dd
-    return fechaConAtraso.format('YY/MM/DD');
-
-
+  // Fecha con atraso calculado
+  const fechaConAtraso = moment().subtract(diasAtraso, 'days');
+  // Regresamos string con fecha en formato yy/mm/dd
+  return fechaConAtraso.format('YY/MM/DD');
 }
 
 // Insertar datos a la base de datos
 function insertarDB(datos) {
-    // Conexión al servidor
-    console.log('Conectando al servidor SQL...')
-    sql.connect(config).then(() => {
-        console.log('Correctamente conectado al servidor SQL.')
-        var request = new sql.Request();
-        // Para cada dato
-        console.log('Inicia proceso de inserción...');
-        datos.forEach(recaudo => {
-            // Construir el SQL
-            let strQuery = 'INSERT INTO ' + tabla + ' (Fecha, recaudador, NoRecaudos, MontoRecaudado, MontoAforado) VALUES(';
-            strQuery += '\''+ recaudo.Fecha +'\',';
-            strQuery += '\''+ recaudo.recaudador +'\',';
-            strQuery += '\''+ recaudo.NoRecaudos +'\',';
-            strQuery += '\''+ recaudo.MontoRecaudado +'\',';
-            strQuery += '\''+ recaudo.MontoAforado +'\'';
-            strQuery += ')';
-            // Ejecutar la consulta
-            request.query(strQuery).catch((err) => {
-                // Imprimir errores en consulta
-                console.log('Request error: ' + err);
-            });
-        });
-        console.log('Fin de proceso de inserción.');
-    }).catch((err) => {
-        // Imprimir errores en la conexión
-        if (err) {
-            console.log('SQL Connection Error: ' + err);
-        }
+  // Conexión al servidor
+  console.log('Conectando al servidor SQL...');
+  sql.connect(config).then(() => {
+    console.log('Correctamente conectado al servidor SQL.');
+    const request = new sql.Request();
+    // Para cada dato
+    console.log('Inicia proceso de inserción...');
+    datos.forEach((recaudo) => {
+      // Construir el SQL
+      let strQuery = 'INSERT INTO ' + tabla + ' (Fecha, recaudador, NoRecaudos, MontoRecaudado, MontoAforado) VALUES(';
+      strQuery += '\''+ recaudo.Fecha +'\',';
+      strQuery += '\''+ recaudo.recaudador +'\',';
+      strQuery += '\''+ recaudo.NoRecaudos +'\',';
+      strQuery += '\''+ recaudo.MontoRecaudado +'\',';
+      strQuery += '\''+ recaudo.MontoAforado +'\'';
+      strQuery += ')';
+      // Ejecutar la consulta
+      request.query(strQuery).catch((err) => {
+        // Imprimir errores en consulta
+        console.log('Request error: ' + err);
+      });
     });
-    console.log('Fin de proceso General.')
+    console.log('Fin de proceso de inserción.');
+  }).catch((err) => {
+    // Imprimir errores en la conexión
+    if (err) {
+      console.log('SQL Connection Error: ' + err);
+    }
+  });
+  console.log('Fin de proceso General.')
 }
 
 /**
@@ -85,15 +83,15 @@ console.log('Días de atraso para obtener los datos: ' + diasAtraso + ' días.')
 const cliente = new ClienteREST();
 // Query principal
 cliente.get(urlDtVax + getStringFecha(), (data) => {
-    // Parsear datos a JSON
-    const resultado = JSON.parse(data);
-    // Verificamos que no haya error en la API
-    if (resultado.success === true) {
-        // Regresamos el array de datos
-        insertarDB(resultado.detail);
-    } else {
-        // Arrojamos un error en caso de problema con query a API
-        throw 'Hubo un error al realizar la consulta a DTVax, revise la URL, ' +
-                'el status de la API o la conexión a Internet.';
-    }
+  // Parsear datos a JSON
+  const resultado = JSON.parse(data);
+  // Verificamos que no haya error en la API
+  if (resultado.success === true) {
+    // Regresamos el array de datos
+    insertarDB(resultado.detail);
+  } else {
+    // Arrojamos un error en caso de problema con query a API
+    throw 'Hubo un error al realizar la consulta a DTVax, revise la URL, ' +
+            'el status de la API o la conexión a Internet.';
+  }
 });
